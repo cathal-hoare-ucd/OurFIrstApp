54cc2e87bb763dbef2fc6bc3730e353d3cecc53c
Cathal Hoare <cathalhoare@gmail.com>
2018-02-19 10:58:54 +0000

    Initial Commit

---
+++ OurFIrstApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OurFIrstApp
+//
+//  Created by Official Things on 19/02/2018.
+//  Copyright © 2018 UCC_CS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
